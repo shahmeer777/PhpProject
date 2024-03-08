215fe8dda24a3f3a3fa4561abf1e869f9ea9c540
Shahmeer Ahmed <58287001+shahmeer777@users.noreply.github.com>
2024-03-08 06:01:49 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$out = null;
+$str = 'hello';
+
+$out = strlen($str);
+$out = str_word_count($str);
+?>
+<!DOCTYPE html>
+<html lang="en"><?= $out ?>
+<head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+</head>
+<body>
+            
+</body>
+</html>
+
+
+
+
+
+
+
+
